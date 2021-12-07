da8614abe6a4933f27ef8b8af31d771b167f04a4
Thomas Kennedy <thomas_kennedy@goodyear.com>
2021-12-07 15:57:13 -0500

    Finished puzzle

--- Day1/solution.ps1
+++ Day1/solution.ps1
@@ -1,17 +1,20 @@
 
-$depthReadings = @(199,200,208,210,200,240,269,260,263)
-$lastReading = 0
-$increaseCnt = 0
+#$depthReadings = @(199,200,208,210,200,207,240,269,260,263)
+$depthReadings = Get-Content .\data.txt
+
+$lastReading = 171
+$increaseCnt = 1
 $decreaseCnt = 0
+$sameCnt     = 0
 
 foreach ($reading in $depthReadings) {
-  #Write-Host "$($reading.gettype())"
-  Write-Host ""
-  Write-Host "LastReading = $lastReading"
-  Write-Host "Reading = $reading"
+  $reading = [int]$reading
+  #Write-Host ""
+  #Write-Host "LastReading = $lastReading"
+  #Write-Host "Reading = $reading"
   
   # If first reading . . .
-  if ($lastReading -eq 0) {
+  if ($lastReading -eq 171) {
     $lastReading = $reading
     continue
   }
@@ -23,9 +26,13 @@ foreach ($reading in $depthReadings) {
     Write-Host "Decreasing . . ."
     $decreaseCnt += 1
   }
+  if ($reading -eq $lastReading) {
+    Write-Host "Same Depth . . ."
+    $sameCnt += 1
+  }
   $lastReading = $reading
 }
 
 return "
-Depth increased $increaseCnt and decreased $decreaseCnt during the readings.
+Depth increased $increaseCnt and decreased $decreaseCnt and stayed the same $sameCnt times during the readings.
 "
