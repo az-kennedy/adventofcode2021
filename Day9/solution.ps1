856aec158b2eec5dad05cb0f1c75393f36958b0a
Thomas Kennedy <doomedsififreak@gmail.com>
2021-12-09 06:58:21 -0500

    Works on test case

--- Day9/solution.ps1
+++ Day9/solution.ps1
@@ -2,7 +2,7 @@
 # Your first goal is to find the low points - the locations that are lower 
 # than any of its adjacent locations.
 
-#$data = Get-Content .\Day9\data.txt
+#$data = Get-Content C:\Users\Thomas\Documents\code\REPOS\adventofcode2021\Day9\data.txt
 
 $data = '2199943210
 3987894921
@@ -20,15 +20,7 @@ foreach ($row in $heightmap) {
   }
 }
 
-$up           = 0
-$down         = 0
-$left         = 0
-$right        = 0
-$previousRow  = ''
-$nextRow      = ''
-
-
-function Find-LowPoint {
+function Find-LowPointRisk {
   param (
     # Previous Row
     [String]
@@ -46,35 +38,53 @@ function Find-LowPoint {
   $PreviousRowCA = $PreviousRow.ToCharArray()
   $CurrentRowCA = $CurrentRow.ToCharArray()
   $NextRowCA = $NextRow.ToCharArray()
+  $rowRisk = 0
 
-  Write-Host "Previous Row: [$PreviousRowCA]"
-  Write-Host "Current Row: [$CurrentRowCA]"
-  Write-Host "Next Row: [$NextRowCA]"
+  #Write-Host "Previous Row: [$PreviousRowCA]"
+  #Write-Host "Current Row: [$CurrentRowCA]"
+  #Write-Host "Next Row: [$NextRowCA]"
   
 
   for ($i = 0; $i -lt $CurrentRowCA.Count; $i++) {
-    $number = $CurrentRowCA[$i]
+    $myArray = @()
+    [int][string]$number = $CurrentRowCA[$i]
+    $myArray += $number
     #Write-Host "Current Number: [$number]"
-    try{$up = $PreviousRowCA[$i]}catch{$up = ''}
-    try{$down = $NextRowCA[$i]}catch{$down = ''}
-    try{$left = $CurrentRowCA[$($i-1)]}catch{$left = ''}
-    try{$right = $CurrentRowCA[$($i+1)]}catch{$right = ''}
-
-    $myArray = @([int][string]$number,[int][string]$up,[int][string]$down,[int][string]$left,[int][string]$right)
+    try{
+      $up = $PreviousRowCA[$i]
+      if($up){$myArray += [int][string]$up}
+    }catch{$up = ''}
+    try{
+      $down = $NextRowCA[$i]
+      if($down){$myArray += [int][string]$down}
+    }catch{$down = ''}
+    try{
+      $left = $CurrentRowCA[$($i-1)]
+      if($left){$myArray += [int][string]$left}
+    }catch{$left = ''}
+    try{
+      $right = $CurrentRowCA[$($i+1)]
+      if($right){$myArray += [int][string]$right}
+    }catch{$right = ''}
     
-    Write-Host "MyArray = [$myArray]"
+    
+    #Write-Host "MyArray = [$myArray]"
 
-    $lowest = ($myArray | Measure-Object).Minimum
+    $lowest = ($myArray | Measure-Object -Minimum).Minimum
+
+    #Write-Host "Lowest : $lowest"
 
     if ($lowest -eq $number) {
-      Write-Host "Low point of [$number]"
-      return $number
+      Write-Host "Low point of [$number], risk level of [$($number+1)]"
+      $rowRisk += $number +1
+      #return $number
     }
   }
   Write-Host ""
+  return $rowRisk
 }
 
-$sum = 0
+$riskSum = 0
 
 for ($i = 0; $i -lt $heightmapNew.Count; $i++) {
   try{
@@ -87,9 +97,9 @@ for ($i = 0; $i -lt $heightmapNew.Count; $i++) {
   $currentRow = $heightmapNew[$i]
   try{$nextRow = $heightmapNew[$i+1]}catch{$nextRow = ''}
 
-  $LowPoint = Find-LowPoint -PreviousRow $previousRow -CurrentRow $currentRow -NextRow $nextRow
-  $sum += $LowPoint
+  $rowRisk = Find-LowPointRisk -PreviousRow $previousRow -CurrentRow $currentRow -NextRow $nextRow
+  $riskSum += $rowRisk
 
 }
 
-return "Sum of low points: [$sum]"
+return "Sum of risk points: [$riskSum]"
